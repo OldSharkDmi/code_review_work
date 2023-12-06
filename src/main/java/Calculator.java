10ef180ec233585269fe4d276e9904dfed93af94
AngryRaccoon <03sidcrafter@gmail.com>
2023-12-06 03:02:30 +0300

    add calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,24 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+
+        public int add(int a, int b){
+            return a + b;
+        }
+        public int dif(int a, int b){
+            return a - b;
+        }
+        public int div(int a, int b){
+            if (b == 0) {
+                throw new IllegalArgumentException("Cannot divide by zero");
+            }
+            return a / b;
+        }
+        public int times(int a, int b){
+            return a * b;
+        }
+        public int solver(){
+            int result = dif(10, 2);
+            result = div(result, 2);
+            return result;
+        }
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
-    }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
-    }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
-    }
-    public int solver(){
-        //TODO inser your realisation in method solver
-    }
-}
+
